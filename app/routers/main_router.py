ee08dc3515d1280ddf6370558bcdb4971f1d51bc
serhii <serhiy.loboda@gmail.com>
2024-06-18 15:24:42 +0300

    added auth- and user-routers to main router

--- app/routers/main_router.py
+++ app/routers/main_router.py
@@ -1,4 +1,9 @@
 from fastapi import APIRouter
 
+from app.routers.auth_router import router as auth_router
+from app.routers.user_router import router as user_router
 
 router = APIRouter()
+
+router.include_router(auth_router)
+router.include_router(user_router)
